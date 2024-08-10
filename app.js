57da41069abfb40cdbc6f1d4e01f08b98a230a6e
saloni1810 <salonip@2312@gmail.com>
2024-08-10 17:24:14 +0530

    file name changed

---
+++ app.js
@@ -0,0 +1,30 @@
+import 'dotenv/config';
+import express from 'express';
+import connectMongoDb from "./connection.js"
+import configureMiddleware from "./middlewear/middlewear.js"
+
+import {restrictUser, checkAuth} from './middlewear/auth.js';
+import urlRoute from "./routes/urlRouter.js"
+import path from "path";
+import staticRoute  from "./routes/staticRouter.js"
+import userRoute from "./routes/userRoutes.js"
+
+const app = express();
+const PORT = process.env.PORT ;
+
+//connection to MongoDB Database
+connectMongoDb(process.env.MONGO_URL )
+
+//middleware
+configureMiddleware(app);
+
+//set the view engine to ejs
+app.set("view engine" , "ejs");
+app.set("views", path.resolve("./views"))
+
+//route
+app.use("/url", restrictUser, urlRoute)
+app.use("/", checkAuth, staticRoute)
+app.use("/user", userRoute)
+
+app.listen(PORT, ()=> console.log(`Server started at port : ${PORT}`));
